6fbba5ab630dbb4f3f4bbf398b22a02573562250
Axe714 <axearzadon@gmail.com>
2022-08-03 12:51:18 -0700

    Fixed responsiveness of navbar, fixed formatting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,24 +10,25 @@ export default function Navbar() {
                 <span className="font-['Pacifico'] text-2xl font-black hover:text-blue-900 transition-all">Allec Arzadon</span>
                 </a>
             </div>
-            {/* large screen nav */}
-            <div id="main-navbar" className="hidden md:flex">
-              <a href="#" id="navbar-1" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">About Me</a>
-              <a href="#" id="navbar-2" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Skills</a>
-              <a href="#" id="navbar-3" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Projects</a>
-              <a href="#" id="navbar-4" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Resume</a>
-              <a href="#" id="navbar-5" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Contact Me</a>
-            </div>
+
+        {/* large screen nav */}
+        <div id="main-navbar" className="hidden md:flex">
+            <a href="#" id="navbar-1" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">About Me</a>
+            <a href="#" id="navbar-2" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Skills</a>
+            <a href="#" id="navbar-3" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Projects</a>
+            <a href="#" id="navbar-4" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Resume</a>
+            <a href="#" id="navbar-5" className="font-['Manrope'] py-5 px-12 text-gray-700 transform hover:scale-110 hover:text-blue-500 transition-all">Contact Me</a>
+        </div>
 
         {/* mobile button */}
-        <div className="inline-flex items-center md:hidden">
+        <div className="flex items-center md:hidden">
             <button className="mobile-menu-button">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </button>
         </div>  
         </div>
     </div>
-    
+
     {/* mobile nav */}
     <div className="bg-white mobile-menu  hidden md:hidden">
         <a href="#" className="font-['Manrope'] block py-2 px-4 text-sm hover:bg-sky-100 hover:text-blue-900 transition-all">About Me</a>
