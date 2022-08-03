921a22263ca9a77c95ae7884d5057739a3ba41b8
Axe714 <axearzadon@gmail.com>
2022-08-03 09:16:08 -0700

    Set-up home.js file, linked components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
+import React from "react";
+import Home from "./Home"
 import './index.css';
 
-function App() {
-  return (
-    <div className="text-9xl">
-      <p>Hi</p>
-    </div>
-  );
-}
+const App = () => <Home />;
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import AboutMe from './components/AboutMe'
+import TechStack from './components/TechStack'
+import Projects from './components/Projects'
+import ContactForm from './components/ContactForm'
+import Footer from './components/Footer'
+
+export default function renderHome() {
+    return (
+        <div className="bg-slate-500 text-9xl">
+          <p>TEST!</p>
+        </div>
+      );
+}
