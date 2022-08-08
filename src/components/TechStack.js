3e996e69b4ad4c126ba0fa10b6504bc3e34e213a
Axe714 <axearzadon@gmail.com>
2022-08-07 20:29:29 -0700

    Added animation to tech stack component

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import HtmlLogo from "../images/icons/html.svg";
 import CSSLogo from "../images/icons/css.svg";
 import JavascriptLogo from "../images/icons/javascript.svg";
@@ -13,18 +16,49 @@ import GithubLogo from "../images/icons/github.svg";
 import SlackLogo from "../images/icons/slack.svg";
 
 export default function TechStack() {
+  // we use this use in view hook that sets a boolean to TRUE or FALSE if element that has ref={ref} is in view or not
+  const { ref, inView } = useInView({
+    // 20% of the element must be present for animation to start
+    threshold: 0.2,
+  });
+  const animation = useAnimation();
+
+  //we use the useEffect hook here to invoke a function when the state of an element CHANGES. We set [inView] as the parameter to monitor
+  // so that ONLY when an element is [inView] === true, then the function below is invoked.
+  useEffect(() => {
+    console.log("use effect hook, inView = ", inView);
+    if (inView) {
+      animation.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 1,
+          bounce: 0.3,
+        },
+      });
+    }
+    // elements are hidden when NOT in view.
+    if (!inView) {
+      animation.start({ x: "-100vw" });
+    }
+  }, [inView]);
+
   return (
     <div className="bg-white grid grid-rows-3 -mt-48 mx-8 divide-y md:grid-rows-none md:grid-cols-3 md:mx-16 md:divide-x rounded-lg outline outline-sky-100">
-      <div className="flex flex-col place-items-center md:justify-center md:items-center text-center h-auto py-5 px-5">
+      <div
+        ref={ref}
+        className="flex flex-col place-items-center md:justify-center md:items-center text-center h-auto py-5 px-5"
+      >
         <span
           id="stack-title-1"
           className="font-['Manrope'] font-bold text-blue-900 pb-8 text-2xl"
         >
           Front-End
         </span>
-        <div
+        <motion.div
           id="stack-icons-1"
           className="grid grid-cols-2 gap-x-10 gap-y-5 md:gap-x-14 md:gap-y-5"
+          animate={animation}
         >
           <img
             src={HtmlLogo}
@@ -52,19 +86,23 @@ export default function TechStack() {
             alt="Tailwind-Logo"
             className="max-h-16 md:max-h-16"
           />
-        </div>
+        </motion.div>
       </div>
 
-      <div className="flex flex-col place-items-center text-center h-auto px-5">
+      <div
+        ref={ref}
+        className="flex flex-col place-items-center text-center h-auto px-5"
+      >
         <span
           id="stack-title-2"
           className="font-['Manrope'] font-bold text-blue-900 text-2xl pt-5 pb-10"
         >
           Back-End
         </span>
-        <div
+        <motion.div
           id="stack-icons-2"
           className="grid grid-cols-2 gap-x-8 md:gap-x-12 gap-y-5"
+          animate={animation}
         >
           <img
             src={ExpressLogo}
@@ -86,17 +124,24 @@ export default function TechStack() {
             alt="MySQL-Logo"
             className="max-h-16 md:max-h-16"
           />
-        </div>
+        </motion.div>
       </div>
 
-      <div className="flex flex-col place-items-center text-center h-auto px-5">
+      <div
+        ref={ref}
+        className="flex flex-col place-items-center text-center h-auto px-5"
+      >
         <span
           id="stack-title-3"
           className="font-['Manrope'] font-bold text-blue-900 text-2xl pt-5 pb-10"
         >
           Dev Tools
         </span>
-        <div id="stack-icons-3" className="grid grid-cols-2 gap-x-12 gap-y-5">
+        <motion.div
+          id="stack-icons-3"
+          className="grid grid-cols-2 gap-x-12 gap-y-5"
+          animate={animation}
+        >
           <img
             src={GithubLogo}
             alt="Github-Logo"
@@ -107,7 +152,7 @@ export default function TechStack() {
             alt="Slack-Logo"
             className="max-h-16 md:max-h-16"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
